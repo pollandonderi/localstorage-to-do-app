1f53a1935df6108099eb027cc5e045b6fa4bf3c4
polland <pollandsabisa@gmail.com>
2023-07-26 14:54:13 +0300

    done

---
+++ index.js
@@ -0,0 +1,25 @@
+//select respective items using the ir selectoer and id
+const ul = document.querySelector('ul');
+const input = document.getElementById('item');
+// now fetch the array from the local storage
+let itemsArray = localStorage.getItem('items');
+// if the items do not exist then evaluate to an empty array
+JSON.parse(localStorage.getItem('items'))||[];
+
+itemsArray.forEach(addTask);
+function addTask(text){
+  const li = document.createElement('li')
+  li.textContent = text;
+  ul.appendChild(li);
+}
+function addItem(){
+    itemsArray.push(input.value);
+    localStorage.setItem('items', JSON.stringify(itemsArray));
+    addTask(input.value);
+    input.value = '';
+  }
+  function clearItem(){
+    localStorage.clear();
+    ul.innerHTML = '';
+    itemsArray = [];
+  }
